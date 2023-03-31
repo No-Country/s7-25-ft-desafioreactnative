610f8394fe700558dd6edcfa3774eb00b31c42e2
David Francisco <davidfco.pozo@gmail.com>
2023-03-31 17:50:54 -0400

    Path de componentes modificadas por reubicacion de carpetas
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/components/OnBoardingView.js
+++ client/src/components/OnBoardingView.js
@@ -1,49 +1,53 @@
-import { View, ActivityIndicator,StyleSheet } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { View, ActivityIndicator, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-import HomeScreen from './HomeScreenPrueba';
-import OnBoarding from './OnBoarding';
-
+import HomeScreen from "../screens/HomeScreenPrueba";
+import OnBoarding from "../navigation/OnBoardingNavigation";
 
 export default function OnBoardingView() {
+  const [loading, setLoading] = useState(true);
+  const [viewedOnboarding, setViewedOnboarding] = useState(false);
 
-    const [loading, setLoading] = useState(true);
-    const [viewedOnboarding, setViewedOnboarding] = useState(false);
+  const Loading = () => {
+    return (
+      <View>
+        <ActivityIndicator size={"large"} color={"blue"} animating={true} />
+      </View>
+    );
+  };
 
-    const Loading = () =>{
-        return(
-            <View>
-                <ActivityIndicator size={'large'} color={'blue'} animating={true}/>
-            </View>
-        );
+  const checkOnboarding = async () => {
+    try {
+      const value = await AsyncStorage.getItem("@viewedOnboarding");
+      if (value !== null) {
+        setViewedOnboarding(true);
+      }
+    } catch (err) {
+      console.log("Error @checkOnboarding: ", err);
+    } finally {
+      setLoading(false);
     }
-
-    const checkOnboarding = async()=>{
-        try{
-          const value = await AsyncStorage.getItem('@viewedOnboarding');
-          if(value!== null){
-            setViewedOnboarding(true);
-          }
-        }catch(err){
-          console.log('Error @checkOnboarding: ',err);
-        }finally{
-          setLoading(false);
-        }
-      };
-      useEffect(()=>{
-        checkOnboarding();
-      },[]);
+  };
+  useEffect(() => {
+    checkOnboarding();
+  }, []);
 
   return (
     <View style={styles.container}>
-    {loading?<Loading/>:viewedOnboarding ? <HomeScreen/>:<OnBoarding/>}
+      {loading ? (
+        <Loading />
+      ) : viewedOnboarding ? (
+        <HomeScreen />
+      ) : (
+        <OnBoarding />
+      )}
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-    container:{
-      flex:1,
-    }
-  })
+  container: {
+    flex: 1,
+  },
+});
