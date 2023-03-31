aeb9ff0c40adc61f6468ec43cadd5dd48d436c6b
David Francisco <davidfco.pozo@gmail.com>
2023-03-31 17:51:42 -0400

    Bottom navigation agregado al stack navigation
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/src/navigation/StackNavigation.js
+++ client/src/navigation/StackNavigation.js
@@ -1,21 +1,25 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SignUp from "../screens/SignUp";
 import SignIn from "../screens/SignIn";
-import OnBoarding1 from "../components/OnBoarding1";
-import OnBoarding from "../components/OnBoarding";
+import OnBoardingNavigation from "./OnBoardingNavigation";
+import BottomTabNavigator from "./BottomTabNavigator";
 
 const Stack = createNativeStackNavigator();
 
 function StackNavigation() {
   return (
     <Stack.Navigator
-      initialRouteName="Onboarding1"
+      initialRouteName="OnBoarding"
       screenOptions={{ headerShown: false }}
     >
       <Stack.Group>
-        <Stack.Screen name="Onboarding1" component={OnBoarding} />
+        <Stack.Screen
+          name="OnBoardingNavigation"
+          component={OnBoardingNavigation}
+        />
         <Stack.Screen name="SignUp" component={SignUp} />
         <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Home" component={BottomTabNavigator} />
       </Stack.Group>
     </Stack.Navigator>
   );
