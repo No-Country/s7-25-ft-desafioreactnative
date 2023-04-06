40eec3694743f0087e1efc1879e2005c64f4c8da
Juan Isaias Meza Conde <102775234+JuanIsaiasMC@users.noreply.github.com>
2023-04-06 12:48:35 -0500

    Revert "Feat/signup screen"

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,22 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./src/screens/**/*.{js,jsx,ts,tsx}",
-    "./src/components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
   ],
   theme: {
-    extend: {},
-    colors: {
-      brandGreen: "#CBFB5E",
-      brandBlue: "#0E0B1F",
-      neutralLightGray: "#EEEEEE",
-      neutralDarkGray: "#71737B",
-      neutralMarineBlue: "#20242F",
-    },
-    extend: {
-    }
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
