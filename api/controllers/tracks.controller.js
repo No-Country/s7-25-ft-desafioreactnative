a6bef1767d49389d2f8f60b9818f22dccd6ff2b5
unknown <nicosepertino@hotmail.com>
2023-04-08 20:17:42 -0300

    agregar query sortBy y sortDirection para ordenamiento ruta get tracks

--- api/controllers/tracks.controller.js
+++ api/controllers/tracks.controller.js
@@ -42,7 +42,7 @@ const uploadTrack = catchAsync(async (req, res, next) => {
 
 const getTracks = catchAsync(async (req, res, next) => {
   try {
-    const { page, search } = req.query;
+    const { page, search, sortBy, sortDirection } = req.query;
     const pageSize = 10;
 
     const offset = (page - 1) * pageSize;
@@ -56,11 +56,18 @@ const getTracks = catchAsync(async (req, res, next) => {
         }
       : {};
 
+    let order = [["createdAt", sortDirection || "DESC"]];
+    if (sortBy === "price") {
+      order = [[sortBy, sortDirection || "ASC"]];
+    } else if (sortBy === "title") {
+      order = [[sortBy, sortDirection || "ASC"]];
+    }
+
     const tracks = await Track.findAll({
       where: filter,
       offset,
       limit,
-      order: [["createdAt", "ASC"]],
+      order
     });
 
     const count = await Track.findAndCountAll({ where: filter });
@@ -98,8 +105,10 @@ const uploadTracksTest = catchAsync(async (req, res, next) => {
         const { title, price } = trackData;
         const track = await Track.create({
           title: title.slice(0, -4),
-          download_url: "https://storage.googleapis.com/soundscaleapp-15d98.appspot.com/audios/041322b4-ccef-4de0-9b58-7eb8fc025aedsmoke-143172.mp3?GoogleAccessId=firebase-adminsdk-dmobp%40soundscaleapp-15d98.iam.gserviceaccount.com&Expires=4102455600&Signature=Hp9WsuYgLale5tIEFRGDO2Uyv4PYp5Tw02Ra951k0AjPza9EO8LdQUDP0D%2FI6hat8X%2FmzVW3g1lMwZvl0obj87AjrYcsAb86S9u1JrAauP6sGWd3Mshm%2FiWp%2BRSe4vA0RDBDoT%2F4Rg4GRtludNA293L4mUAO8FsmHdtAPTXOxVNRCtyvuESvwxMeoyZYp3h3%2BEQ1BdGosDER7%2FhLm%2BEu5wTYbm0Taudi6yUBmce497mTDC0X8eQBxFGvfLS6s4iHqPtP7KBz1z8c57xrfFbAqb4WtsguBYqy3EQ3uw%2FyYmJxjPxkmt9NXKiEuE%2FSgIvyj3KRX7MQ0V23V72IPFqL2Q%3D%3D",
-          image_url: "https://storage.googleapis.com/soundscaleapp-15d98.appspot.com/images/041322b4-ccef-4de0-9b58-7eb8fc025aedimagenSmokeMusic.jpg?GoogleAccessId=firebase-adminsdk-dmobp%40soundscaleapp-15d98.iam.gserviceaccount.com&Expires=4102455600&Signature=W26vXvBZGbBFlwRQsf0g7%2Bm5RXlOWfsuUPUEdmIiD0r01KjyASmiTiUEoO2jGzra0JXa5okss6OK3TThfdlGuQxE4hg7z2W0nWHI7gwCZaYbLbKr%2Bv6yGguIbMxbDK2h0M1UQBAdLeakk%2BTq%2Fif2VoK0SXfUFY%2F3dxXeGyqpy%2FM8WUyVaP3xMr95qiBlL3ecMO3faUhL9RyC28%2F0HUTsediXRa3FSQ2ruGV44BYj8scLTiwPkzB%2B42PGPERRmlrU1brYGVITMv8ZramJcPfamF0xumH6ahXQFWPHdGhK6gKiCfL1YAg9xYm3ujXtcs1Tth7yylnQozuc5SRG12YGwg%3D%3D",
+          download_url:
+            "https://storage.googleapis.com/soundscaleapp-15d98.appspot.com/audios/041322b4-ccef-4de0-9b58-7eb8fc025aedsmoke-143172.mp3?GoogleAccessId=firebase-adminsdk-dmobp%40soundscaleapp-15d98.iam.gserviceaccount.com&Expires=4102455600&Signature=Hp9WsuYgLale5tIEFRGDO2Uyv4PYp5Tw02Ra951k0AjPza9EO8LdQUDP0D%2FI6hat8X%2FmzVW3g1lMwZvl0obj87AjrYcsAb86S9u1JrAauP6sGWd3Mshm%2FiWp%2BRSe4vA0RDBDoT%2F4Rg4GRtludNA293L4mUAO8FsmHdtAPTXOxVNRCtyvuESvwxMeoyZYp3h3%2BEQ1BdGosDER7%2FhLm%2BEu5wTYbm0Taudi6yUBmce497mTDC0X8eQBxFGvfLS6s4iHqPtP7KBz1z8c57xrfFbAqb4WtsguBYqy3EQ3uw%2FyYmJxjPxkmt9NXKiEuE%2FSgIvyj3KRX7MQ0V23V72IPFqL2Q%3D%3D",
+          image_url:
+            "https://storage.googleapis.com/soundscaleapp-15d98.appspot.com/images/041322b4-ccef-4de0-9b58-7eb8fc025aedimagenSmokeMusic.jpg?GoogleAccessId=firebase-adminsdk-dmobp%40soundscaleapp-15d98.iam.gserviceaccount.com&Expires=4102455600&Signature=W26vXvBZGbBFlwRQsf0g7%2Bm5RXlOWfsuUPUEdmIiD0r01KjyASmiTiUEoO2jGzra0JXa5okss6OK3TThfdlGuQxE4hg7z2W0nWHI7gwCZaYbLbKr%2Bv6yGguIbMxbDK2h0M1UQBAdLeakk%2BTq%2Fif2VoK0SXfUFY%2F3dxXeGyqpy%2FM8WUyVaP3xMr95qiBlL3ecMO3faUhL9RyC28%2F0HUTsediXRa3FSQ2ruGV44BYj8scLTiwPkzB%2B42PGPERRmlrU1brYGVITMv8ZramJcPfamF0xumH6ahXQFWPHdGhK6gKiCfL1YAg9xYm3ujXtcs1Tth7yylnQozuc5SRG12YGwg%3D%3D",
           price,
         });
         await user.addTrack(track);
