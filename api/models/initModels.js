55a70127b4f51a1336705b74e9c46b1f2abdb7b2
unknown <nicosepertino@hotmail.com>
2023-04-09 13:55:51 -0300

    Definir modelo Genre y relacion muchos a muchos con modelo Track

--- api/models/initModels.js
+++ api/models/initModels.js
@@ -3,11 +3,27 @@
 const { User } = require('./user.model');
 const { Profile } = require('./profile.model');
 const { Track } = require('./track.model');
+const { Genre } = require('./genre.model');
 const initModels = () => {
+
 	User.hasOne(Profile);
 	Profile.belongsTo(User);
+
   User.hasMany(Track, { foreignKey: "user_id" });
   Track.belongsTo(User, { foreignKey: "user_id" });
+
+  Track.belongsToMany(Genre, {
+    through: "track_genre",
+    foreignKey: "track_id",
+    otherKey: "genre_id",
+    timestamps: false,
+  });
+  Genre.belongsToMany(Track, {
+    through: "track_genre",
+    foreignKey: "genre_id",
+    otherKey: "track_id",
+    timestamps: false,
+  });
 };
 
-module.exports = { initModels, User, Track };
+module.exports = { initModels, User, Track, Genre };
