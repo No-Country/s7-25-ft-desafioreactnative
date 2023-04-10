7dc61b3c57c67d723d7c109dddb9b245874a047c
unknown <nicosepertino@hotmail.com>
2023-04-09 21:56:48 -0300

    agregar generos a db al iniciar servidor

--- api/models/initModels.js
+++ api/models/initModels.js
@@ -1,13 +1,12 @@
 // Establish your models relations inside this function
 // Models
-const { User } = require('./user.model');
-const { Profile } = require('./profile.model');
-const { Track } = require('./track.model');
-const { Genre } = require('./genre.model');
-const initModels = () => {
-
-	User.hasOne(Profile);
-	Profile.belongsTo(User);
+const { User } = require("./user.model");
+const { Profile } = require("./profile.model");
+const { Track } = require("./track.model");
+const { Genre } = require("./genre.model");
+const initModels = async () => {
+  User.hasOne(Profile);
+  Profile.belongsTo(User);
 
   User.hasMany(Track, { foreignKey: "user_id" });
   Track.belongsTo(User, { foreignKey: "user_id" });
@@ -24,6 +23,33 @@ const initModels = () => {
     otherKey: "track_id",
     timestamps: false,
   });
+
+  const musicGenres = [
+    "rock",
+    "pop",
+    "jazz",
+    "hip hop",
+    "reggaeton",
+    "blues",
+    "country",
+    "folk",
+    "electronic",
+    "classical",
+    "rap",
+    "r&b",
+    "metal",
+    "salsa",
+    "merengue",
+    "bachata",
+    "cumbia",
+    "funk",
+    "soul",
+    "disco"
+  ];
+
+  const promises = musicGenres.map((genreName) => Genre.findOrCreate({where: {name: genreName}}))
+  await Promise.all(promises);
+
 };
 
 module.exports = { initModels, User, Track, Genre };
