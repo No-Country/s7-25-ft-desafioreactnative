d23c076a496de4c143a48133e47689a484eaf9bc
nicoseper89 <nicosepertino@hotmail.com>
2023-04-18 16:58:31 -0300

    arreglo filtro generos get pistas

--- api/controllers/tracks.controller.js
+++ api/controllers/tracks.controller.js
@@ -12,6 +12,7 @@ const {
 } = require("../models/initModels");
 const { v4: uuidv4 } = require("uuid");
 const { Op } = require("sequelize");
+const { db } = require("../utils/database.util");
 const { formatDuration, getMetadata } = require("../utils/metadata.util");
 const env = require("dotenv").config();
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
@@ -93,7 +94,7 @@ const getTracks = catchAsync(async (req, res, next) => {
         {
           model: Genre,
           as: "genres",
-          attributes: ["name"],
+          attributes: [],
           where:
             genres.length > 0 && Array.isArray(genres)
               ? {
@@ -102,17 +103,34 @@ const getTracks = catchAsync(async (req, res, next) => {
                   },
                 }
               : {},
-        },
-        {
-          model: User,
-          as: "artist",
-          attributes: ["userName", "email"],
-        },
+        }
       ],
       offset,
       limit,
       order,
     });
+
+    const resArray = await Promise.all(
+      tracks.map(async (track) =>
+        Track.findByPk(track.id, {
+          include: [
+            {
+              model: Genre,
+              as: "genres",
+              attributes: ["name"],
+              through: {
+                attributes: [],
+              },
+            },
+            {
+              model: User,
+              as: "artist",
+              attributes: ["userName", "email"],
+            },
+          ],
+        })
+      )
+    );
 
     const count = await Track.findAndCountAll({
       where: filter,
@@ -141,7 +159,7 @@ const getTracks = catchAsync(async (req, res, next) => {
     res.status(200).json({
       status: "success",
       data: {
-        tracks,
+        tracks: resArray,
         pagination: {
           pageSize,
           page,
