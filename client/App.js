31aa57d790f28e7c5ddbe8b2111069a62aa7eda7
David Francisco <davidfco.pozo@gmail.com>
2023-04-19 23:00:19 +0200

    Variables de entornos agregadas
    Signed-off-by: David Francisco <davidfco.pozo@gmail.com>

--- client/App.js
+++ client/App.js
@@ -9,7 +9,6 @@ import { NavigationContainer } from "@react-navigation/native";
 import StackNavigation from "./src/navigation/StackNavigation";
 import GenresSelect from "./src/screens/GenresSelect";
 import axios from "axios";
-import { api, STRIPE_KEY } from "@env";
 import MainNavigation from "./src/navigation/MainNavigation";
 import { StatusBar } from "expo-status-bar";
 import { init } from "./src/helpers/audioControllers";
@@ -25,6 +24,7 @@ NativeWindStyleSheet.setOutput({
 
 // url base a partir de la cual axios va a realizar las llamadas al back
 axios.defaults.baseURL = Constants.expoConfig.extra.api;
+const STRIPE_KEY = Constants.expoConfig.extra.STRIPE_KEY;
 
 export default function App() {
   useEffect(() => {
